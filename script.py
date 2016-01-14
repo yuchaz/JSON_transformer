d03f3f1e586bc416db7ca087d1ca5620845e8b90
yuchaz <erwin.yuchia.chen@gmail.com>
2016-01-14 20:04:57 +0800

    change the json schema

--- script.py
+++ script.py
@@ -4,11 +4,12 @@ filename = []
 
 with open('./filename.txt') as inputData:
 	filename = inputData.read().split("\n")
-	filename.remove("")
+	# filename.remove("")
+	# print(filename)
 
 with open('./load.json') as jsonObj:
 	objJSON = json.load(jsonObj)
 	for name in filename:
-		objJSON['data'].append({"filename": name, "directory": "./img/"})
+		objJSON['data'].append({"src": name, "details": "nothing to tell"})
 	with open('./output/load.json', 'w+') as outputFile:
 		json.dump(objJSON, outputFile)
