4a6ae5c301bfdcbad45bdf656683985df493e2ae
YuChia <erwin.yuchia.chen@gmail.com>
2016-01-18 13:02:27 +0800

    Add more feature

--- script.py
+++ script.py
@@ -7,9 +7,15 @@ with open('./filename.txt') as inputData:
 	# filename.remove("")
 	# print(filename)
 
-with open('./load.json') as jsonObj:
-	objJSON = json.load(jsonObj)
-	for name in filename:
-		objJSON['data'].append({"src": name, "details": "nothing to tell"})
-	with open('./output/load.json', 'w+') as outputFile:
-		json.dump(objJSON, outputFile)
+
+	
+for idx in range(1,4):
+	with open('./load.json') as jsonObj:
+		objJSON = json.load(jsonObj)
+		for name in filename:
+			nwname = "http://psyop.hopto.org/yuchazImg/"+str(idx)+"/"+name
+			print(nwname)
+			objJSON['data'].append({"src": nwname, "details": "nothing to tell"})
+		objJSON['current_page'] = idx
+		with open('./output/'+str(idx)+'.json', 'w+') as outputFile:
+			json.dump(objJSON, outputFile)
